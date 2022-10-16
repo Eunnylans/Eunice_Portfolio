254a3bee223cbf1d6220c681b13873e2ffd533e0
Eunice Igbinedion <eunice.igbinedion@yahoo.com>
2022-10-16 17:02:22 +0200

    Adding Particles Component

--- src/Components/Particles.js
+++ src/Components/Particles.js
@@ -1,66 +1,67 @@
-import React from "react";
 import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+//import { loadFull } from "tsparticles";
+import { useCallback, useMemo } from "react";
 
+const ParticlesComponent = (props) => {
+  // using useMemo is not mandatory, but it's recommended since this value can be memoized if static
+  const options = useMemo(() => {
+    // using an empty options object will load the default options, which are static particles with no background and 3px radius, opacity 100%, white color
+    return {
+      background: {
+        color: "white", // this sets a background color for the canvas
+      },
+      fullScreen: {
+        enable: true, // enabling this will make the canvas fill the entire screen, it's enabled by default
+        zIndex: -3, // this is the z-index value used when the fullScreen is enabled, it's 0 by default
+      },
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true, // enables the click event
+            mode: "push", // adds the particles on click
+          },
+          onHover: {
+            enable: true, // enables the hover event
+            mode: "repulse", // make the particles run away from the cursor
+          },
+        },
+        modes: {
+          push: {
+            quantity: 10, // number of particles to add on click
+          },
+          repulse: {
+            distance: 100, // distance of the particles from the cursor
+          },
+        },
+      },
+      particles: {
+        links: {
+          enable: true, // enabling this will make particles linked together
+          distance: 200, // maximum distance for linking the particles
+        },
+        move: {
+          enable: true, // enabling this will make particles move in the canvas
+          speed: { min: 1, max: 5 }, // using a range in speed value will make particles move in a random speed between min/max values, each particles have its own value, it won't change in time by default
+        },
+        opacity: {
+          value: { min: 0.3, max: 0.7 }, // using a different opacity, to have some semitransparent effects
+        },
+        size: {
+          value: { min: 1, max: 3 }, // let's randomize the particles size a bit
+        },
+      },
+    };
+  }, []);
 
+  // useCallback is not mandatory, but it's recommended since this callback can be memoized if static
+  const particlesInit = useCallback((engine) => {
+    loadSlim(engine);
+    // loadFull(engine); // for this sample the slim version is enough, choose whatever you prefer, slim is smaller in size but doesn't have all the plugins and the mouse trail feature
+  }, []);
 
-Particles = () => (
-<Particles
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 8,
-	            "density": {
-	                "enable": true,
-	                "value_area": 800
-	            }
-	        },
-	        "line_linked": {
-	            "enable": false
-	        },
-	        "move": {
-	            "speed": 1,
-	            "out_mode": "out"
-	        },
-	        "shape": {
-	            "type": [
-	                "image",
-	                "circle"
-	            ],
-	            "image": [
-	                {
-	                    "src": "/react.cd2ab268.svg",
-	                    "height": 20,
-	                    "width": 23
-	                },
-	                {
-	                    "src": "/k8s.2d579d24.svg",
-	                    "height": 20,
-	                    "width": 20
-	                },
-	                {
-	                    "src": "/code.b3b4c4f4.png",
-	                    "height": 20,
-	                    "width": 20
-	                }
-	            ]
-	        },
-	        "color": {
-	            "value": "#CCC"
-	        },
-	        "size": {
-	            "value": 30,
-	            "random": false,
-	            "anim": {
-	                "enable": true,
-	                "speed": 4,
-	                "size_min": 10,
-	                "sync": false
-	            }
-	        }
-	    },
-	    "retina_detect": false
-	}} />
+  // setting an id can be useful for identifying the right particles component, this is useful for multiple instances or reusable components
+  return <Particles id={props.id} init={particlesInit} options={options} />;
+};
 
-    );
-
-export default Particles;
+export default ParticlesComponent;
